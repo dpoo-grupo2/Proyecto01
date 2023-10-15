8c47d706f4072e9a28c7e71df46fb5a0c45f227b
Guillermo Hernández <g.hernandeze@uniandes.edu.co>
2023-10-15 18:56:03 -0500

    getters seguro

--- Proyecto1_RentaCarros/src/modelo/Seguro.java
+++ Proyecto1_RentaCarros/src/modelo/Seguro.java
@@ -1,7 +1,26 @@
 package modelo;
 
-public class Seguro
-{
-	private String nombreSeguro;
-	private int valorSeguro;
+public class Seguro {
+    private String nombreSeguro;
+    private int valorSeguro;
+
+    
+    public String getNombreSeguro() {
+        return nombreSeguro;
+    }
+
+    
+    public void setNombreSeguro(String nombreSeguro) {
+        this.nombreSeguro = nombreSeguro;
+    }
+
+    
+    public int getValorSeguro() {
+        return valorSeguro;
+    }
+
+    
+    public void setValorSeguro(int valorSeguro) {
+        this.valorSeguro = valorSeguro;
+    }
 }
