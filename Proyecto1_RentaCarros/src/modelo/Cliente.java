c8de6289e36f53ed217072a9c4ee13fe9070774d
Guillermo Hernández <g.hernandeze@uniandes.edu.co>
2023-10-15 18:53:37 -0500

    super clase usuario a cliente

--- Proyecto1_RentaCarros/src/modelo/Cliente.java
+++ Proyecto1_RentaCarros/src/modelo/Cliente.java
@@ -1,34 +1,35 @@
 package modelo;
 
-public class Cliente 
+public class Cliente extends Usuario
 {
-private Integer edad; 
-private String cedula;
-private String correoElectronico;
-private String fechaNacimiento;
+	private Integer edad; 
+	private String cedula;
+	private String correoElectronico;
+	private String fechaNacimiento;
 
-Cliente(Integer edad,String cedula, String correoElectronico,String fechaNacimiento) // falta implementar usuario
-{
-	this.edad = edad;
-	this.cedula = cedula;
-	this.correoElectronico = correoElectronico;
-	this.fechaNacimiento = fechaNacimiento;
-}
-
-public Integer getEdad() {
-	return this.edad;
-}
-public String getCedula()
-{
-	return this.cedula;
-}
-public String getCorreo() 
-{
-	return this.correoElectronico;
-}
-public String getFechaN() 
-{
-	return this.fechaNacimiento;	
-}
+	public Cliente(String login, String password, String nombreCompleto,Integer edad,String cedula, String correoElectronico,String fechaNacimiento) // falta implementar usuario
+	{
+		super(login, password, nombreCompleto);
+		this.edad = edad;
+		this.cedula = cedula;
+		this.correoElectronico = correoElectronico;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	public Integer getEdad() {
+		return this.edad;
+	}
+	public String getCedula()
+	{
+		return this.cedula;
+	}
+	public String getCorreo() 
+	{
+		return this.correoElectronico;
+	}
+	public String getFechaNacimiento() 
+	{
+		return this.fechaNacimiento;	
+	}
 
 }
