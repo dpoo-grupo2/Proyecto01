67ef7122a9d16480450d32dfc9219c4d7da1507e
Jaimeemj <123424910+Jaimeemj@users.noreply.github.com>
2023-10-15 20:16:01 -0500

    Licencia cons y getters

--- Proyecto1_RentaCarros/src/modelo/LicenciaConduccion.java
+++ Proyecto1_RentaCarros/src/modelo/LicenciaConduccion.java
@@ -6,6 +6,28 @@ public class LicenciaConduccion
    private String paisExpedicion;
    private String fechaVencimiento;
    private String imagenLicencia; // toca ver q hacemos con esto
-    
+   public LicenciaConduccion(int numero, String paisExpedicion, String fechaVencimiento, String imagenLicencia) 
+   {
+	this.numero = numero;
+	this.paisExpedicion = paisExpedicion;
+	this.fechaVencimiento = fechaVencimiento;
+	this.imagenLicencia = imagenLicencia;
+   }
+   public int getNumero() 
+   {
+	   return this.numero;
+   }
+   public String paisExpedicion() 
+   {
+	   return this.paisExpedicion;
+   }
+   public String fechaVencimiento() 
+   {
+	   return this.fechaVencimiento;
+   }
+   public String imagenLicencia() 
+   {
+	   return this.imagenLicencia;
+   }
 }
 
