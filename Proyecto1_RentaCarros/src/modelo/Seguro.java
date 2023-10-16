0e7e2345b467aea61c7ddece17b00e6071fb8c34
Guillermo Hernández <g.hernandeze@uniandes.edu.co>
2023-10-15 19:02:40 -0500

    seguro constructor

--- Proyecto1_RentaCarros/src/modelo/Seguro.java
+++ Proyecto1_RentaCarros/src/modelo/Seguro.java
@@ -1,26 +1,23 @@
 package modelo;
 
-public class Seguro {
+public class Seguro 
+{
     private String nombreSeguro;
     private int valorSeguro;
 
+    public Seguro(String nombreSeguro, int valorSeguro) 
+    {
+    	this.nombreSeguro = nombreSeguro;
+    	this.valorSeguro = valorSeguro;
+    }
     
     public String getNombreSeguro() {
         return nombreSeguro;
     }
-
-    
-    public void setNombreSeguro(String nombreSeguro) {
-        this.nombreSeguro = nombreSeguro;
-    }
-
-    
+ 
     public int getValorSeguro() {
         return valorSeguro;
     }
-
     
-    public void setValorSeguro(int valorSeguro) {
-        this.valorSeguro = valorSeguro;
-    }
+    
 }
