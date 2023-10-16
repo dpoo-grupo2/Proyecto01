d1af0b370bd5ec352371fb7f857a8fbdaca23130
Guillermo Hernández <g.hernandeze@uniandes.edu.co>
2023-10-15 19:21:11 -0500

    adminlocal contrs y get

--- Proyecto1_RentaCarros/src/modelo/AdministradorLocal.java
+++ Proyecto1_RentaCarros/src/modelo/AdministradorLocal.java
@@ -1,5 +1,17 @@
 package modelo;
 
-public class AdministradorLocal {
+public class AdministradorLocal 
+{
 
+	private String nombre;
+	
+	public AdministradorLocal(String nombre) 
+	{
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() 
+	{
+        return nombre;
+    }
 }
